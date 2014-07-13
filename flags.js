39d9903f6f772ad3072f4a9e776f80ded08869cf
Magda <magda.cejner@gmail.com>
2014-07-13 13:32:57 +0200

    first commit

---
+++ flags.js
@@ -0,0 +1,60 @@
+/**
+ * Created by magda on 12/07/14.
+ */
+
+
+var flags = [
+{ name:"albania", file:"albania.png" },
+{ name:"andorra", file:"andorra.png" },
+{ name:"anglia", file:"anglia.png" },
+{ name:"austria", file:"austria.png" },
+{ name:"belgia", file:"belgia.png" },
+{ name:"bialorus", file:"bialorus.png" },
+{ name:"bosniaihercegowina", file:"bosniaihercegowina.png" },
+{ name:"bulgaria", file:"bulgaria.png" },
+{ name:"chorwacja", file:"chorwacja.png" },
+{ name:"czarnogora", file:"czarnogora.png" },
+{ name:"czechy", file:"czechy.png" },
+{ name:"dania", file:"dania.png" },
+{ name:"estonia", file:"estonia.png" },
+{ name:"finlandia", file:"finlandia.png" },
+{ name:"francja", file:"francja.png" },
+{ name:"grecja", file:"grecja.png" },
+{ name:"hiszpania", file:"hiszpania.png" },
+{ name:"holandia", file:"holandia.png" },
+{ name:"irlandia", file:"irlandia.png" },
+{ name:"islandia", file:"islandia.png" },
+{ name:"kosovo", file:"kosovo.png" },
+{ name:"liechtenstein", file:"liechtenstein.png" },
+{ name:"litwa", file:"litwa.png" },
+{ name:"lotwa", file:"lotwa.png" },
+{ name:"luksemburg", file:"luksemburg.png" },
+{ name:"macedonia", file:"macedonia.png" },
+{ name:"malta", file:"malta.png" },
+{ name:"moldawia", file:"moldawia.png" },
+{ name:"monako", file:"monako.png" },
+{ name:"niemcy", file:"niemcy.png" },
+{ name:"norwegia", file:"norwegia.png" },
+{ name:"polska", file:"polska.png" },
+{ name:"portugalia", file:"portugalia.png" },
+{ name:"rosja", file:"rosja.png" },
+{ name:"rumunia", file:"rumunia.png" },
+{ name:"sanmarino", file:"sanmarino.png" },
+{ name:"serbia", file:"serbia.png" },
+{ name:"slowacja", file:"slowacja.png" },
+{ name:"slowenia", file:"slowenia.png" },
+{ name:"szwajcaria", file:"szwajcaria.png" },
+{ name:"szwecja", file:"szwecja.png" },
+{ name:"turcja", file:"turcja.png" },
+{ name:"ukraina", file:"ukraina.png" },
+{ name:"watykan", file:"watykan.png" },
+{ name:"wegry", file:"wegry.png" },
+{ name:"wlochy", file:"wlochy.png" }
+    ];
+
+
+$(function() {
+
+ $("#flag").attr("src", "flags/wegry.png");
+
+});
